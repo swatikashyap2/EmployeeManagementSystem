8b60c8f4657440cb169e703ed035fd8f4ee7c7a0
Aksa Sandal <asandal@oneims.com>
2024-01-22 11:46:23 +0530

    change in user_leave_type tabel

--- db/migrate/20240109102834_create_user_leave_types.rb
+++ db/migrate/20240109102834_create_user_leave_types.rb
@@ -1,8 +1,8 @@
 class CreateUserLeaveTypes < ActiveRecord::Migration[7.0]
   def change
     create_table :user_leave_types do |t|
-      t.integer :user_id
-      t.integer :leave_type_id
+      t.integer :user_id, foreign_key: true
+      t.integer :leave_type_id, foreign_key: true
       
       t.timestamps
     end
